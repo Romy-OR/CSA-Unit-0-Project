ce5645ebf54d1c0deb3655cce41a3980a0b36f7e
Romy-OR <131908@epsstudents.org>
2024-08-25 20:27:52 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,43 @@
+public class Main {
+  public static void main(String[] args) 
+  { 
+    System.out.println("LDLDLDLDLDLDLDLDLDLDLDLDLD LDLDLDLDLDLDLDLDLDLDLDLDLDLD LDLDLDLDLDLDLDLDLDLDLDLDLD");
+    System.out.println("LD            LDLDLDLDLDLD LDLDLDLDLD        LDLDLDLDLD LD            LDLDLDLDLDLD");
+    System.out.println("LD      LD      LDLDLDLDLD LDLDLD      LDLD      LDLDLD LD      LD      LDLDLDLDLD");
+    System.out.println("LD      LDLD      LDLDLDLD LDLD    LDLDLDLDLDLD    LDLD LD      LDLD      LDLDLDLD");
+    System.out.println("LD      LDLDLD      LDLDLD LD    LDLDLDLDLDLDLDLD    LD LD      LDLDLD      LDLDLD");
+    System.out.println("LD      LDLD      LDLDLDLD LD    LDLDLDLDLDLDLDLD    LD LD      LDLD      LDLDLDLD");
+    System.out.println("LD      LD      LDLDLDLDLD LD    LDLDLDLDLDLDLDLD    LD LD      LD      LDLDLDLDLD");
+    System.out.println("LD            LDLDLDLDLDLD LD    LDLDLDLDLDLDLDLD    LD LD            LDLDLDLDLDLD");
+    System.out.println("LD      LD      LDLDLDLDLD LD    LDLDLDLDLDLDLDLD    LD LD      LD      LDLDLDLDLD");
+    System.out.println("LD      LDLD      LDLDLDLD LDLD    LDLDLDLDLDLD    LDLD LD      LDLD      LDLDLDLD");
+    System.out.println("LD      LDLDLD      LDLDLD LDLDLD      LDLD      LDLDLD LD      LDLDLD      LDLDLD");
+    System.out.println("LD      LDLDLDLD      LDLD LDLDLDLDLD        LDLDLDLDLD LD      LDLDLDLD      LDLD");
+    System.out.println("LDLDLDLDLDLDLDLDLDLDLDLDLD LDLDLDLDLDLDLDLDLDLDLDLDLDLD LDLDLDLDLDLDLDLDLDLDLDLDLD");
+
+
+
+    //I tried to do a for loop inside a for loop to make it easier but I couldn't figure it out
+    
+    //int i = 0;
+    //while (i <20) {
+    //   System.out.print("LD");
+    //    i += 1;
+  
+    //}
+   // List<Integer> spaces = new ArrayList<Integer>();
+   // spaces.add(0,1,1,1,0,1,1,0);
+    
+  //  for (int j = 0; j < spaces.size(); j++) {
+    //  for (int i = 0; i < spaces.size(); i++) {
+  //      if (i.equals(1)){
+   //       System.out.print("LD");
+   //     }else{
+  //        System.out.print("  ");
+  //      }
+  //    }
+  //  }
+
+    
+  }
+}
